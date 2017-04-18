0dbf471cc8871751307f49f97f800f6e0e7f1945
es54288 <es54288@LACCHLBQW7L382.lac.nsroot.net>
2017-04-18 12:17:54 -0300

    Initial checking of sales tool.

---
+++ GitTest/src/SalesData/Main.java
@@ -0,0 +1,22 @@
+package SalesData;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData salesData = new SalesData();
+		displayGreeting();
+		
+		salesData.Display();
+
+	}
+
+	private static void displayGreeting() {
+	    System.out.println("Greetings!! happy sales People");
+	    System.out.println("This show sales data");
+		
+	    
+	    
+	}
+	
+
+}
---
+++ GitTest/src/SalesData/SalesData.java
@@ -0,0 +1,11 @@
+package SalesData;
+
+public class SalesData {
+
+	public void Display() {
+		// TODO Auto-generated method stub
+		System.out.println("data display");
+		
+	}
+
+}
